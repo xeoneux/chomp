7c25496105669cd0da6c363086d0dbd71e7137a7
Aayush Kapoor <xeoneux@icloud.com>
2016-05-30 01:11:57 +0530

    feat: add websocket server

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+///<reference path="typings/index.d.ts"/>
+
 const os = require("os");
 const ip = require("ip");
+const ws = require("ws");
 const dns = require("dns");
 const arp = require("arpjs");
+const http = require("http");
 const portscanner = require("portscanner");
 
 const config = {
@@ -10,42 +14,50 @@ const config = {
 };
 
 // Step 1: Get list of connected IP addresses
-arp.table((error, devices) => {
-    // Step 2: Get status of port on those IP addresses
-    let address = scanPorts(devices);
-    if (address) {
-        // Server exists on one of the IP addresses
-        // Step 3A: Connect to the server.
-        // TODO: Connect to WebSocket Server
-    } else {
-        // Server does not exists on one of the connected IP addresses
-        // Step 3B: Create a server.
-        // TODO: Create new WebSocket Server
-
-        // Step 4B: Check ports again after server creation
-        let address = scanPorts(devices);
+async function server() {
+    await arp.table(async(error, devices) => {
+        // Step 2: Get status of port on those IP addresses
+        let address = await scanPorts(devices);
         if (address) {
             // Server exists on one of the IP addresses
-            // Step 5B: Compare IP addresses
-            if (ip.toLong(config.ip) > ip.toLong(address)) {
-                // TODO: Destroy created server and repeat whole process
+            // Step 3A: Connect to the server.
+            const wsc = new ws(`ws://${address}`);
+        } else {
+            // Server does not exists on one of the connected IP addresses
+            // Step 3B: Create a server.
+            const server = http.createServer();
+            const wss = new ws.Server({server});
+            server.listen(config.port, () => {
+                console.log(`Listening on ${server.address().port}`);
+            });
+
+            // Step 4B: Check ports again after server creation
+            let address = scanPorts(devices);
+            if (address) {
+                // Server exists on one of the IP addresses
+                // Step 5B: Compare IP addresses
+                if (ip.toLong(config.ip) > ip.toLong(address)) {
+                    // TODO: Destroy created server and repeat whole process
+                }
             }
         }
-    }
-});
+    });
+}
 
 async function getUserIPAddress() {
-    let ip;
+    let ip = null;
     await dns.lookup(os.hostname(), (err, add, fam) => ip = add);
     return ip;
 }
 
-function scanPorts(addresses): number {
-    let ip;
-    addresses.forEach(async(address) => {
-        await portscanner.checkPortStatus(config.port, address, (error, status) => {
-            if (address !== config.ip && status !== "closed") ip = address;
+async function scanPorts(devices) {
+    let ip = null;
+    await devices.forEach(async(device) => {
+        await portscanner.checkPortStatus(config.port, device.ip, (error, status) => {
+            if (device.ip !== config.ip && status !== "closed") ip = device.ip;
         });
     });
     return ip;
 }
+
+server();
