fa3e3c6439f065e9b7790d2fde48def11fceb070
Aayush Kapoor <xeoneux@icloud.com>
2016-05-30 21:52:57 +0530

    fix: move port scanning logic to utilities

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
 ///<reference path="typings/index.d.ts"/>
 
-const os = require("os");
 const ip = require("ip");
-const dns = require("dns");
 const arp = require("arpjs");
 const primus = require("primus");
-const portscanner = require("portscanner");
 
 import config from "./config";
-import {getLocalAddress} from "./utilities";
+import {portScanner, getLocalAddress} from "./utilities";
 
 // Step 1: Get list of connected IP addresses
 async function server() {
     await arp.table(async(error, devices) => {
         // Step 2: Get status of port on those IP addresses
-        let address = await scanPorts(devices);
-        if (address) {
+        if (true) {
             // Server exists on one of the IP addresses
             // Step 3A: Connect to the server.
         } else {
@@ -27,26 +23,15 @@ async function server() {
             });
 
             // Step 4B: Check ports again after server creation
-            let address = scanPorts(devices);
-            if (address) {
+            if (true) {
                 // Server exists on one of the IP addresses
                 // Step 5B: Compare IP addresses
-                if (ip.toLong(getLocalAddress()) > ip.toLong(address)) {
+                if (ip.toLong(getLocalAddress()) > ip.toLong("ADDRESS")) {
                     // TODO: Destroy created server and repeat whole process
                 }
             }
         }
     });
-}
-
-async function scanPorts(devices) {
-    let ip = null;
-    for (let device of devices) {
-        await portscanner.checkPortStatus(config.port, device.ip, (error, status) => {
-            if (device.ip !== getLocalAddress() && status !== "closed") ip = device.ip;
-        });
-    }
-    return ip;
 }
 
 server();
--- utilities.ts
+++ utilities.ts
@@ -1,5 +1,8 @@
 const os = require("os");
 const dns = require("dns");
+const portscanner = require("portscanner");
+
+import config from "./config";
 
 export async function getLocalAddress() {
     return await new Promise((resolve, reject) => {
@@ -7,5 +10,18 @@ export async function getLocalAddress() {
             if (err) reject(err);
             else resolve(add);
         });
+    });
+}
+
+export async function portScanner(addresses) {
+    let localAddress = await getLocalAddress();
+    return await new Promise((resolve, reject) => {
+        for (let address of addresses) {
+            portscanner.checkPortStatus(config.port, address, (error, status) => {
+                if (error) reject(error);
+                if (address !== localAddress && status !== "closed") resolve(address);
+            });
+        }
+        resolve(null);
     });
 }
