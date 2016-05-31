985d2402ff6401e1598995277677851e92d20eee
Aayush Kapoor <xeoneux@icloud.com>
2016-05-31 10:15:23 +0530

    feat: add electron prebuilt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import server from "./server";
+import {app, BrowserWindow} from "electron";
 
-server();
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600, frame: false});
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
