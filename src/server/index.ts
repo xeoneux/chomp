f380262c5969b753aa63b5ca19318e30dc63fc18
Aayush Kapoor <xeoneux@icloud.com>
2016-06-01 22:04:29 +0530

    feat: add debug for logging

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,21 @@
 const ip = require("ip");
-const http = require("http");
+const debug = require("debug");
 const socket = require("socket.io");
 
 import config from "../config";
 import {scanPorts} from "../utilities";
 
 export default async function server() {
+    debug("server")("init");
     let localAddress = config.address;
     let serverAddress = await scanPorts();
     if (serverAddress) return serverAddress;
     else return new Promise(async(resolve, reject) => {
+        debug("server")("create socket.io server");
         let io = socket();
         io.listen(config.port);
         registerServerMethods(io);
-        // Check for server again...
+        debug("server")("check for existing server");
         let serverAddress = await scanPorts();
         if (serverAddress) {
             if (ip.toLong(localAddress) > ip.toLong(serverAddress)) {
