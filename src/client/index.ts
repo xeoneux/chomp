2ddf5e573a62aeeb59570451bde87015ea450517
Aayush Kapoor <xeoneux@icloud.com>
2016-06-07 00:54:15 +0530

    feat: upload file on drag and drop

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,4 @@
+const dragDrop = require("drag-drop");
 const io = require("socket.io-client");
 const WebTorrent = require("webtorrent");
 
@@ -5,17 +6,23 @@ import {ipcRenderer} from "electron";
 
 let url = ipcRenderer.sendSync("url");
 let socket = io(url);
+let client = new WebTorrent();
 
 socket.on("hello", (data) => {
     console.log(data);
 });
 
 socket.on("download", (magnet) => {
-    let client = new WebTorrent();
     client.add(magnet, (torrent) => {
         console.log("Client is downloading:", torrent.infoHash);
         torrent.files.forEach((file) => {
             file.appendTo("body");
         });
+    });
+});
+
+dragDrop("body", (files) => {
+    client.seed(files, (torrent) => {
+        console.log("Client is seeding:", torrent.infoHash);
     });
 });
