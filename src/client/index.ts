fbc479d7e8caa6e8e9ad6f6f2d0c75e1e2ac4b56
Aayush Kapoor <xeoneux@icloud.com>
2016-06-07 00:49:02 +0530

    feat: add client side webtorrent code

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,21 @@
 const io = require("socket.io-client");
+const WebTorrent = require("webtorrent");
 
 import {ipcRenderer} from "electron";
 
 let url = ipcRenderer.sendSync("url");
 let socket = io(url);
 
-socket.on("hello", function (data) {
+socket.on("hello", (data) => {
     console.log(data);
+});
+
+socket.on("download", (magnet) => {
+    let client = new WebTorrent();
+    client.add(magnet, (torrent) => {
+        console.log("Client is downloading:", torrent.infoHash);
+        torrent.files.forEach((file) => {
+            file.appendTo("body");
+        });
+    });
 });
